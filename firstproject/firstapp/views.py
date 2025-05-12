a5ec4ae65d0c5363de44f45a1fea5dde346d0540
Ren <rg326@live.com>
2025-05-11 23:51:47 -0400

    added request to return index.html in views.py

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -22,3 +22,5 @@ def home(request):
         if form.is_valid():
             form.save()
             return HttpResponse("Success")
+        
+    return render(request, "index.html", {"form": form})
