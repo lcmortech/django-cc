916ceb74d8eac813eaa5b43d482c0eed28b15eda
Ren <rg326@live.com>
2025-05-14 20:16:50 -0400

    created and added paths to urls.py

--- project/firstapp/views.py
+++ project/firstapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+def hello_world(request):
+    return HttpResponse("hello world")
+
+class HelloNYC(View):
+    def get(self, request):
+        return HttpResponse("Hello NYC")
