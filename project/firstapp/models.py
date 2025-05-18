94a2ef284a14193d32ae697c089abf994351644d
Ren <rg326@live.com>
2025-05-17 23:55:03 -0400

    added comments field to reservation model

--- project/firstapp/models.py
+++ project/firstapp/models.py
@@ -10,3 +10,4 @@ class Reservation(models.Model):
     last_name=models.CharField(max_length=255)
     guest_count=models.IntegerField()
     reservation_time=models.DateField(auto_now=True)
+    comments=models.TextField(max_length=1000)
