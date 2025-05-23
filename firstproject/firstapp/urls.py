fe1402eaa20ab8ec4d7bf45bd2442ac1130d8481
Ren <rg326@live.com>
2025-05-22 23:17:15 -0400

    added forms.py

--- firstproject/firstapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('function', views.hello_world),
-    path('class', views.HelloNYC.as_view()),
-    path('reservation', views.home),
-]
